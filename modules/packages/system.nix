3ca906f5255e44d3c6addce8462299a68fbe6927
Xellor-Dev <xellor.cooperation@gmail.com>
2026-02-05 20:58:40 +0100

    docs: format code in system.nix for improved readability

--- modules/packages/system.nix
+++ modules/packages/system.nix
@@ -11,12 +11,12 @@
     programs.firefox.enable = true;
 
     programs.nh = {
-    enable = true;
-    # Путь к твоей папке с конфигами (важно для работы из любого места)
-    flake = "/home/xellor/nixos-config"; 
-    # Автоматическая очистка каждую неделю
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
+      enable = true;
+      # Путь к твоей папке с конфигами (важно для работы из любого места)
+      flake = "/home/xellor/nixos-config";
+      # Автоматическая очистка каждую неделю
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
     };
 
     # Allow unfree packages
